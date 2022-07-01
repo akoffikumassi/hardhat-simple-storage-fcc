d3e4a8cbd98106deba3572a026aad1667b8f87d1
Yada dev <yadadev.io@gmail.com>
2022-07-01 00:21:54 -0400

    refactoring

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -24,7 +24,7 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {},
-    rinkby: {
+    rinkeby: {
       url: RINKEBY_RPC_URL,
       accounts: [PRIVATE_KEY],
       chainId: 4,
